404a7657a65232ee99ed13757002c3d6a2b24a14
ajidik <ajidikhogor@yandex.ru>
2024-09-11 17:44:15 +0300

    Add files via upload

---
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,37 @@
+﻿
+#include <iostream>
+int high = 0;
+int shirina = 9;
+int shirina_2 = 9;
+int i_2 = 0;
+
+int main()
+{
+    std::cin >> high;
+        for (int i = 0; i <= ((high-1))+1; i++) {
+            std::cout << ' ';
+        }
+        std::cout << '*';
+        for (int i = 0; i <= high; i++) {
+            if (i == high) {
+                std::cout << '\n';
+                for (int c = 0; c <= high*2+2; c++) {
+                    std::cout << '*';
+                }
+            }
+            else {
+                std::cout << '\n';
+                for (int a = 1; a <= (high)-i; a++) {
+                    std::cout << ' ';
+                }
+                std::cout << '*';
+                for (int b = 0; b <= i * 2; b++) {
+                    std::cout << ' ';
+                }
+                std::cout << '*';
+            }
+
+        }
+    
+    
+}
